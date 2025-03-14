2e663a2cbdbfe6d4bd71da7513ada643aa24ab13
Nilesh <nilesh.jaiswar006@gmail.com>
2025-03-15 00:01:39 +0530

    Miscellaneous changes (logo, css)

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>Copyright @ 2025, Cryptoplace - All right Reserved</p>
+      <p>Copyright @ 2025, Coin Traker - All right Reserved</p>
     </div>
   )
 }
